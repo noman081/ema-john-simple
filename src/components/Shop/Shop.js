9b1753e0d4cb1845a995d7c13c3d13c9c47eb4a5
noman081 <abdullahalnoman642@gmail.com>
2022-03-22 21:43:02 +0600

    After adding headers

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Shop.css';
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="product-container">
+                
+            </div>
+            <div className="cart-container">
+                <h3>Order Summary</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
